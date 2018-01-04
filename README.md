9d74bf97cad9fae24c9f33f78ed46bf540523014
gianluciano <gian@xl7.com.br>
2018-01-04 15:22:53 -0200

    alterando gráficos

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # d3-studies
+
+My code learning d3 from the book: Scott Murray-Interactive Data Visualization for the Web-O'Reilly Media (2013)
--- script.js
+++ script.js
@@ -77,8 +77,28 @@ svg.selectAll("text")
   .attr("fill","white")
   .attr("text-anchor","middle");
 
-// quarto
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+// quarto
+//variaveis
 var dataset = [
   [5,20],
   [480,90],
@@ -89,17 +109,28 @@ var dataset = [
   [475,44],
   [25,67],
   [85,21],
-  [220,88]
+  [220,88],
+  [600,150]
 ];
+var w = 500;
+var h = 300;
+var padding = 20
 
-var xScale = d3.scale.linear()
+//escalas
+var xScale = d3.scaleLinear()
   .domain([0,d3.max(dataset,function(d){return d[0]})])
-  .range([0,w]);
+  .range([padding,w - padding * 2]);
 
-var yScale = d3.scale.linear()
+var yScale = d3.scaleLinear()
   .domain([0,d3.max(dataset,function(d){return d[1]})])
-  .range([0,h]);
+  .range([h - padding,padding]);
 
+var rScale = d3.scaleLinear()
+.domain([0,d3.max(dataset,function(d){return d[1]})])
+.range([2,5]);
+
+
+//plotting
 var svg = d3.select("body").append("svg");
 svg.attr("width",w).attr("height",h);
 
@@ -114,9 +145,10 @@ svg.selectAll("circle")
   return yScale(d[1]);
 })
 .attr("r",function(d,i){
-  return Math.sqrt(h-d[1]);
+  return rScale(d[1]);
 });
 
+//texto
 svg.selectAll("text")
 .data(dataset)
 .enter()
@@ -125,11 +157,16 @@ svg.selectAll("text")
   return d[0]+","+d[1];
 })
 .attr("x",function(d){
-  return d[0];
+  return xScale(d[0]);
 })
 .attr("y",function(d){
-  return d[1];
+  return yScale(d[1]);
 })
 .attr("font-family","sans-serif")
 .attr("font-size","11px")
 .attr("fill","red");
+
+//axes
+var xAxis = d3.axisBottom(xScale);
+
+svg.append("g").attr("class","axis").call(xAxis);
