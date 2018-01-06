877ad356f0a5fa4d58e31154ae17445e3bdafb4a
Gian Carlo <gian.luciano@acad.pucrs.br>
2018-01-06 21:28:39 -0200

    adicionando quinto gráfico

--- script.js
+++ script.js
@@ -78,43 +78,27 @@ svg.selectAll("text")
   .attr("text-anchor","middle");
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
 
 
   
 // quarto
 //variaveis
-var dataset = [
-  [5,20],
-  [480,90],
-  [250,50],
-  [100,33],
-  [330,95],
-  [410,12],
-  [475,44],
-  [25,67],
-  [85,21],
-  [220,88],
-  [600,150]
-];
+var dataset = [];
+var numDataPoints = 50;
+var xRange = Math.random() * 1000;
+var yRange = Math.random() * 1000;
+for(var i = 0; i < numDataPoints ; i++){
+  var newNum1 = Math.floor(Math.random() * xRange);
+  var newNum2 = Math.floor(Math.random() * yRange);
+  dataset.push([newNum1,newNum2])
+}
+
+
+
 var w = 500;
 var h = 300;
-var padding = 20
+var padding = 30
 
 //escalas
 var xScale = d3.scaleLinear()
@@ -148,25 +132,119 @@ svg.selectAll("circle")
   return rScale(d[1]);
 });
 
-//texto
-svg.selectAll("text")
-.data(dataset)
-.enter()
-.append("text")
-.text(function(d){
-  return d[0]+","+d[1];
-})
-.attr("x",function(d){
-  return xScale(d[0]);
-})
-.attr("y",function(d){
-  return yScale(d[1]);
-})
-.attr("font-family","sans-serif")
-.attr("font-size","11px")
-.attr("fill","red");
 
 //axes
-var xAxis = d3.axisBottom(xScale);
+var xAxis = d3.axisBottom(xScale).ticks(5);
 
-svg.append("g").attr("class","axis").call(xAxis);
+svg.append("g")
+  .attr("class","axis")
+  .attr("transform","translate(0,"+(h-padding)+")")
+  .call(xAxis);
+
+var yAxis = d3.axisLeft(yScale);
+
+svg.append("g")
+  .attr("class","axis")
+  .attr("transform","translate("+padding+",0)")
+  .call(yAxis);
+
+  
+d3.select('body').append("p").text("clique aqui");
+//quinto
+
+var w = 600;
+var h = 250;
+
+var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+        11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+var xScale = d3.scaleBand()
+        .domain(d3.range(dataset.length))
+        .rangeRound([0, w])
+        .paddingInner(0.05);
+var yScale = d3.scaleLinear()
+        .domain([0, d3.max(dataset)])
+        .range([0, h]);
+
+//Create SVG element
+var svg = d3.select("body")
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h);
+//Create bars
+svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("x", function(d, i) {
+        return xScale(i);
+    })
+    .attr("y", function(d) {
+        return h - yScale(d);
+    })
+    .attr("width", xScale.bandwidth())
+    .attr("height", function(d) {
+        return yScale(d);
+    })
+    .attr("fill", function(d) {
+    return "rgb(0, 0, " + Math.round(d * 10) + ")";
+    });
+//Create labels
+svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text(function(d) {
+        return d;
+    })
+    .attr("text-anchor", "middle")
+    .attr("x", function(d, i) {
+        return xScale(i) + xScale.bandwidth() / 2;
+    })
+    .attr("y", function(d) {
+        return h - yScale(d) + 14;
+    })
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "11px")
+    .attr("fill", "white");
+
+    var clicks = 0
+d3.select('p').on("click",function() { 
+  clicks++;
+  
+  if (clicks % 2 == 0) {
+    dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+      11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+  } else {
+    dataset = [11,12,15,20,18,17,16,18,23,25,5,10,13,19,21,25,22,18,15,13]
+  }
+
+
+  svg.selectAll("rect")
+    .data(dataset)
+    .transition()
+    .duration(function(d, i) { return 1000 + i * 100; })
+    .attr("y", function(d) {
+      return h - yScale(d);
+    })
+    .attr("height", function(d) {
+      return yScale(d);
+    })
+    .attr("fill", function(d) {
+      return "rgb(0, 0, " + Math.round(d * 10) + ")";
+    });
+
+  svg.selectAll("text")
+    .data(dataset)
+    .transition()
+    .duration(function(d, i) { return 1000 + i * 100; })
+    .text(function(d) {
+      return d;
+    })
+    .attr("x", function(d, i) {
+      return xScale(i) + xScale.bandwidth() / 2;
+    })
+    .attr("y", function(d) {
+      return h - yScale(d) + 14;
+    })
+
+})
